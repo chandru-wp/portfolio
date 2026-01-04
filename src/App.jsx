b347cf9fb5b45fa50623e0e9e4d78601aea15f44
Chandru <ck6582293@gmail.com>
2026-01-04 16:18:35 +0530

    resume download error fix 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
- import Home from "./pages/Home";
- import AdminDashboard from "./components/AdminDashboard";
+import Home from "./pages/Home";
+import AdminDashboard from "./components/AdminDashboard";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const path = typeof window !== 'undefined' ? window.location.pathname : '/';
-  if (path === '/admin') return <AdminDashboard />;
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
